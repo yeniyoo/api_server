b5a704e71f9c6ccd00aa2791b65f0233e0ba81cb
Nayak <wjdduq914@gmail.com>
2016-07-27 02:06:14 +0900

    [2016.07.27] close #10

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from utils import createResponseData, fbGraphApi
@@ -10,7 +12,11 @@ from users.models import MyUser
 @api_view(['POST'])
 def facebookAuth(request):
     if request.method == 'POST':
-        access_token = request.POST['access_token']
+        try:  # request data check
+            access_token = request.data['access_token']
+        except:
+            return Response(createResponseData(1, "Invalid parameter", None),
+                            status=status.HTTP_400_BAD_REQUEST)
         fb_info = fbGraphApi(access_token)
         if 'id' in fb_info:
             user = MyUser.objects.filter(fb_id=fb_info['id'], is_active=1)
@@ -27,9 +33,10 @@ def facebookAuth(request):
                         status=status.HTTP_500_INTERNAL_SERVER_ERROR
                     )
             # 로그인
-            # TODO: 토큰값이 고정됨. 뭔가 solt값이나 다른 방식이 필요
-            token = Token.objects.update_or_create(user=user[0])
-            return Response(createResponseData(0, "success", None), headers={"auth-token": token[0]})
+            token = Token.objects.get_or_create(user=user[0])
+            Token.objects.filter(key=token[0]).delete()  # 토큰이 새로 생성되던 기존에 존재하던 한번 지우고
+            token = Token.objects.create(user=user[0])  # 새로운 토큰을 부여함. (로그인 할때마다 재생성)
+            return Response(createResponseData(0, "success", None), headers={"auth-token": str('Token ' + str(token))})
         else:
             return Response(
                 createResponseData(1, "incorrect access_token", None),
@@ -38,8 +45,17 @@ def facebookAuth(request):
 
 
 @api_view(['PUT'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def ageSetting(request):
-    if request.method == 'PUT':  # 더미
+    if request.method == 'PUT':
+        try:  # request data check
+            age = request.data['age']
+        except:
+            return Response(createResponseData(1, "Invalid parameter", None),
+                            status=status.HTTP_400_BAD_REQUEST)
+        # age setting
+        MyUser.objects.filter(fb_id=request.user).update(age=age)
         return Response(createResponseData(0, "success", None))
 
 
