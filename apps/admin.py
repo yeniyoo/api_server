2bfcf8b45b22c0148739b83b4d44f1ba9d5ca473
Changhyun Lee <chang12@snu.ac.kr>
2016-07-27 09:19:42 +0900

    [2016.07.27] Modify - Register BackgroundModel to Admin Site

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Round, BackgroundImage
+
+
+# Define ModelAdmins
+class BackgroundImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "image"]
+    list_display_links = ['image']
+
 # Register your models here.
+admin.site.register(BackgroundImage, BackgroundImageAdmin)
