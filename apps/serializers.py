205b376ff63b2bc7bbec2432ef0a4b8dc23016a2
Changhyun Lee <chang12@snu.ac.kr>
2016-08-12 01:41:40 +0900

    #57 구현 - URI 패턴을 수정하고, CommentLike 생성/삭제를 하나의 view로 합침

--- apps/serializers.py
+++ apps/serializers.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F
 
 from rest_framework import serializers
 from rest_framework.exceptions import NotFound
@@ -132,17 +133,17 @@ class RecommentSerializer(serializers.ModelSerializer):
 class CommentLikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommentLike
-        fields = ("comment", )
+        exclude = ("user", "comment", )
 
-    # user field를 채워주기 위해서 create 메소드를 오버라이딩
+    # 오버라이딩
     def create(self, validated_data):
-        # 클라이언트에게 받은 데이터로 CommentLike 인스턴스 생성
-        comment_like = CommentLike(**validated_data)
-        # Request에서 user 정보 추출
-        comment_like.user = self.context.get("request").user
-        # Comment의 like 필드값을 업데이트
-        comment_like.comment.like += 1
-        comment_like.comment.save()
-        # CommentLike를 생성
-        comment_like.save()
+        # CommentLike 인스턴스를 생성
+        # user 정보는 request에서, comment 정보는 URI pattern에서 획득
+        comment_like = CommentLike.objects.create(
+            user=self.context.get("request").user,
+            comment=Comment.objects.get(id=self.context.get("view").kwargs["comment_id"])
+        )
+        # 해당 Comment의 like 필드값을 update
+        # 더 좋은 SQL 구문 하용을 유도하도록 F expression 사용
+        Comment.objects.filter(id=comment_like.comment_id).update(like=F("like") + 1)
         return comment_like
